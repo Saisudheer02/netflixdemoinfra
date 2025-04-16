4e49decfa1c8e42a49c24a46d7feb31326e818c8
Saisudheer02 <nandikollasaisudheer@gmail.com>
2025-04-16 15:03:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 5
-ami = "ami-0ddc798b3f1a5117e"
+ami = "ami-07a6f770277670015"
 instance_type = "t2.medium"
-key_name = "swikp"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+key_name = "saikeypair"
+vpc_security_group_ids = ["sg-044677a817a6391d7"]
 tags = {
 Name = var.instance_names[count.index]
 }
